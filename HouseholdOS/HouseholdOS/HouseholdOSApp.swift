a592fdcc6cd1e3b85877d65b90d5b186217f6a36
codegardener <sharpster@hotmail.com>
2026-08-29 13:07:29 -0700

    Initial swift app project

---
+++ HouseholdOS/HouseholdOS/HouseholdOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  HouseholdOSApp.swift
+//  HouseholdOS
+//
+//  Created by Scott Sharpe on 8/29/26.
+//
+
+import SwiftUI
+
+@main
+struct HouseholdOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
